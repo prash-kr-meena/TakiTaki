68276228870879ae11f1ba815582b2f873895eb0
prashant_meena <prashantme@zeta.tech>
2022-08-03 14:18:35 +0530

    corrected the test case for home-page, as it is now being server through configuration class

--- src/test/java/com/meena/takitaki/HomeControllerTest.java
+++ src/test/java/com/meena/takitaki/HomeControllerTest.java
@@ -12,14 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
 
   @Test
-  public void testHome() throws Exception {
+  public void testHomePage() throws Exception {
     mockMvc.perform(get(create("/")))
         .andExpect(status().isOk())
         .andExpect(view().name("home"))
