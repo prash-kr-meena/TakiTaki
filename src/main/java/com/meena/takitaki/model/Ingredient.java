d13e558b9fd961369334de31442d8e7a996516a0
prashant_meena <prashantme@zeta.tech>
2022-08-07 18:15:27 +0530

    Adding Persistable Implementation for Spring Data JDBC

--- src/main/java/com/meena/takitaki/model/Ingredient.java
+++ src/main/java/com/meena/takitaki/model/Ingredient.java
@@ -1,18 +1,22 @@
 package com.meena.takitaki.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table
-public class Ingredient {
+public class Ingredient implements Persistable<String> {
   @Id
   private String id;
   private String name;
   private Type type;
+
+  @Override
+  public boolean isNew() {
+    return id == null; // probably not the right implementation
+  }
 
   public enum Type {
     WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
